4d2d6a45635103d055175a1cc2896f9c66d2304b
zengqw <ciwei80@126.com>
2015-08-22 22:41:28 +0800

    Revert "Revert "修改ip""
    This reverts commit bc62a653b82b83b0585e442b030bec4e4d67bdb7.

--- src/main/java/com/jedis/TestRedis.java
+++ src/main/java/com/jedis/TestRedis.java
@@ -23,7 +23,7 @@ public class TestRedis {
     @Before
     public void setup() {
         //连接redis服务器，192.168.0.100:6379
-        jedis = new Jedis("127.0.0.1", 6379);
+        jedis = new Jedis("192.168.0.107", 6379);
         //权限认证
         jedis.auth("admin");  
     }
